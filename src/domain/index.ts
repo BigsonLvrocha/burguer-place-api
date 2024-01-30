d9f5e78e47eaaaa3fd85497eb98cc998076e4747
Luiz Victor Linhares Rocha <luiz.rocha@encora.com>
2024-01-30 17:45:23 -0300

    refactor: add index file to domain

---
+++ src/domain/index.ts
@@ -0,0 +1,5 @@
+export { IngredientAmount } from './ingredient-amount.js';
+export { IngredientStorageService } from './ingredient-storage-service.js';
+export { Recipe } from './recipe.js';
+export { StoreRepository, StoreRepositoryToken } from './store-repository.js';
+export { Store } from './store.js';
